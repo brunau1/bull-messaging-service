01260d151f27999dfc85b8996272e4ad5afdf47b
Bruno Santos <brunosantosprotec@gmail.com>
2024-06-12 17:14:29 -0300

    feat: create public interface for queue service

---
+++ messaging/bull/bull-work-queues.port.ts
@@ -0,0 +1,22 @@
+import { QueuePrefixes } from 'src/infrastructure/adapters/messaging/bull/enums/queue-prefixes.enum';
+import { BullWorkQueueConfig } from 'src/infrastructure/adapters/messaging/bull/schemas/bull-work-queue-config';
+
+export type GenericConsumerProcessor = (job: unknown) => Promise<unknown>;
+
+export abstract class BullWorkQueuesPort {
+  abstract addJob(
+    queueName: string,
+    job: unknown,
+    jobOptions?: { queueIdentity: QueuePrefixes; delay?: number },
+  ): Promise<void>;
+  abstract refreshQueuesConfig(fromDatabase?: boolean): Promise<void>;
+  abstract addConsumerProcessor(
+    queuePrefix: QueuePrefixes,
+    processor: GenericConsumerProcessor,
+  ): void;
+  abstract addQueueConfig(queueConfig: BullWorkQueueConfig): Promise<void>;
+  abstract editQueueConfig(queueConfig: BullWorkQueueConfig): Promise<void>;
+  abstract deleteQueueConfig(queueName: string): Promise<void>;
+  abstract createConsumer(queueName: string): void;
+  abstract deleteConsumer(queueName: string, shouldCloseQueue?: boolean): Promise<void>;
+}
